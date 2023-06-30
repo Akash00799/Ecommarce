6be156fed9eafa8d1dfc2dc8eddf339133e05578
Akash <akash@gmail.com>
2023-06-30 15:41:55 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <Home></Home>,
+  // },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage></SignUpPage>,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
